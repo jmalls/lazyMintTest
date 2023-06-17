b76016f59aedb7b95c9d6e43a005966cd643fdec
Jon <jonathanp.mallows@gmail.com>
2023-06-17 18:46:05 +0200

    Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script>
 
 import { BrowserProvider, Contract } from 'ethers';
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 import abi from './abi.json';
 
-const contractAddress = "";
+const contractAddress = "0xE132E26c9d66cDd01d87e530695969B50Ccbfa90";
 
 async function lazyMint() {
     const provider = new BrowserProvider(window.ethereum);
@@ -11,14 +12,8 @@ async function lazyMint() {
     provider.send("eth_requestAccounts");
 
     const signer = await provider.getSigner();
-
-    const contract = new Contract(abi, contractAddress, signer);
-    try {
-        const tx = await contract.lazyMint(1, 0, 0);
-    } catch (error) {
-        console.error(error)
-    }
-    await tx.wait();
+    
+    const sdk = ThirdwebSDK.fromSigner(signer);
 }
 
 </script>
---
+++ src/routes/test.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+
+    async function init() {
+        const contract = await sdk.getContract("{{0xE132E26c9d66cDd01d87e530695969B50Ccbfa90}}");
+
+        const contract.claim()
+    }
+</script>
